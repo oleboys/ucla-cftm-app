269080ad8aa775557bba36be3d623d6f0e285dcc
Roy Kim <1997.roy@gmail.com>
2015-05-29 14:17:00 -0700

    Dial consisting of image buttons.

--- ucla-cftm-app/app/src/main/java/oleboys/ucla_cftm_app/View/DashboardFragment.java
+++ ucla-cftm-app/app/src/main/java/oleboys/ucla_cftm_app/View/DashboardFragment.java
@@ -7,9 +7,14 @@ package oleboys.ucla_cftm_app.View;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewOverlay;
+import android.view.ViewParent;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -22,6 +27,12 @@ import oleboys.ucla_cftm_app.R;
  * A placeholder fragment containing a simple view.
  */
 public class DashboardFragment extends Fragment {
+    private  ImageButton btnWater;
+    private  ImageButton btnEnergy;
+    private  ImageButton btnCarbonEmission;
+    private  ImageButton btnRecycle;
+    private  ImageButton btnFood;
+    private  ImageButton btnPolitics;
     /**
      * The fragment argument representing the section number for this
      * fragment.
@@ -56,7 +67,61 @@ public class DashboardFragment extends Fragment {
                 new DataPoint(4,6)
         });
         graph.addSeries(series);
+        ListenerOnButton(rootView);
         return rootView;
+
+    }
+
+    private void ListenerOnButton(View view){
+        //water
+        btnWater = (ImageButton) view.findViewById(R.id.water);
+        btnWater.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Animate button to position to the top button on the dial.
+            }
+        });
+        //energy
+        btnEnergy = (ImageButton)view.findViewById(R.id.energy);
+        btnEnergy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Animate button to position to the top button on the dial.
+            }
+        });
+        //carbon emission
+        btnCarbonEmission = (ImageButton)view.findViewById(R.id.carbonEmission);
+        btnCarbonEmission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Animate button to position to the top button on the dial.
+            }
+        });
+        //waste and recycle
+        btnRecycle = (ImageButton)view.findViewById(R.id.recycle);
+        btnRecycle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Animate button to position to the top button on the dial.
+            }
+        });
+        //food
+        btnFood = (ImageButton)view.findViewById(R.id.food);
+        btnFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Animate button to position to the top button on the dial.
+            }
+        });
+        //politics
+        btnPolitics = (ImageButton)view.findViewById(R.id.politics);
+        btnPolitics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Animate button to position to the top button on the dial.
+            }
+        });
+
     }
 
     @Override
